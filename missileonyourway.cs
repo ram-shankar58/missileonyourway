d3f76f48a56f42b14cb7b46d2a5a83025c4b975a
R Ram Shankar <110959980+ram-shankar58@users.noreply.github.com>
2023-12-23 17:54:51 +0530

    Update missileonyourway.cs

--- missileonyourway.cs
+++ missileonyourway.cs
@@ -1,168 +1,86 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-namespace MissileDefense
+public class Enemy : DrawableGameComponent
 {
-    // Missile class
-    public class Missile : DrawableGameComponent
+    private Texture2D texture;
+    private Vector2 position;
+    private Rectangle rectangle;
+    private SoundEffect explosionSound;
+    private bool isDestroyed;
+
+    public bool IsDestroyed
     {
-        private SpriteBatch spriteBatch;
-        private Texture2D texture;
-        private Rectangle rectangle;
-        private Random random;
+        get { return isDestroyed; }
+    }
 
-        public Missile(Game game) : base(game)
-        {
-            spriteBatch = game.Services.GetService<SpriteBatch>();
-            texture = game.Content.Load<Texture2D>("missile");
-            random = new Random();
-            rectangle = new Rectangle(random.Next(0, Game.Window.ClientBounds.Width - 10), Game.Window.ClientBounds.Height, 10, 20);
-        }
+    public Rectangle Rectangle
+    {
+        get { return rectangle; }
+    }
 
-        public override void Update(GameTime gameTime)
-        {
-            rectangle.Y -= 5;
-            if (rectangle.Y < 0)
-            {
-                this.Dispose();
-            }
-            base.Update(gameTime);
-        }
+    public Enemy(Game game, Texture2D texture, Vector2 position, SoundEffect explosionSound) : base(game)
+    {
+        this.texture = texture;
+        this.position = position;
+        rectangle = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+        this.explosionSound = explosionSound;
+        isDestroyed = false;
+    }
 
-        public override void Draw(GameTime gameTime)
-        {
-            spriteBatch.Begin();
-            spriteBatch.Draw(texture, rectangle, Color.Red);
-            spriteBatch.End();
-            base.Draw(gameTime);
-        }
+    public void TakeDamage(int damage)
+    {
+        // Handle enemy damage logic
+        // For example, update health and check if destroyed
+        // Replace this with your actual logic
+        // For example:
+        // health -= damage;
+        // if (health <= 0)
+        // {
+        //     isDestroyed = true;
+        // }
 
-        public Rectangle Rectangle
+        // If destroyed, play explosion sound and trigger particle effect
+        if (isDestroyed)
         {
-            get { return rectangle; }
+            explosionSound.Play();
+            Game1 game = (Game1)Game;
+            game.PlayExplosionEffect(position);
         }
     }
 
-    // Player class
-    public class Player : DrawableGameComponent
+    public override void Update(GameTime gameTime)
     {
-        private SpriteBatch spriteBatch;
-        private Texture2D texture;
-        private Rectangle rectangle;
+        // Update logic for the enemy
+        // ...
 
-        public Player(Game game) : base(game)
-        {
-            spriteBatch = game.Services.GetService<SpriteBatch>();
-            texture = game.Content.Load<Texture2D>("player");
-            rectangle = new Rectangle(Game.Window.ClientBounds.Width / 2 - 25, Game.Window.ClientBounds.Height - 60, 50, 50);
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            KeyboardState keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.Left) && rectangle.Left > 0)
-            {
-                rectangle.X -= 5;
-            }
-            if (keyboardState.IsKeyDown(Keys.Right) && rectangle.Right < Game.Window.ClientBounds.Width)
-            {
-                rectangle.X += 5;
-            }
-            base.Update(gameTime);
-        }
-
-        public override void Draw(GameTime gameTime)
-        {
-            spriteBatch.Begin();
-            spriteBatch.Draw(texture, rectangle, Color.Blue);
-            spriteBatch.End();
-            base.Draw(gameTime);
-        }
-
-        public Rectangle Rectangle
-        {
-            get { return rectangle; }
-        }
+        base.Update(gameTime);
     }
 
-    // Game class
-    public class Game1 : Game
+    public override void Draw(GameTime gameTime)
     {
-        private GraphicsDeviceManager graphics;
-        private SpriteBatch spriteBatch;
-        private List<Missile> missiles;
-        private Player player;
-        private SpriteFont font;
-        private int score;
+        // Draw the enemy
+        // ...
 
-        public Game1()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            graphics.PreferredBackBufferWidth = 800;
-            graphics.PreferredBackBufferHeight = 600;
-            missiles = new List<Missile>();
-            score = 0;
-        }
+        base.Draw(gameTime);
+    }
 
-        protected override void Initialize()
-        {
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            Services.AddService<SpriteBatch>(spriteBatch);
-            player = new Player(this);
-            Components.Add(player);
-            font = Content.Load<SpriteFont>("font");
-        }
-
-        protected override void UnloadContent()
-        {
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            if (Keyboard.GetState().IsKeyDown(Keys.Space))
-            {
-                Missile missile = new Missile(this);
-                missiles.Add(missile);
-                Components.Add(missile);
-            }
-
-            foreach (Missile missile in missiles)
-            {
-                if (player.Rectangle.Intersects(missile.Rectangle))
-                {
-                    missile.Dispose();
-                    score++;
-                    Console.WriteLine("Missile intercepted!");
-                }
-            }
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.White);
-
-            spriteBatch.Begin();
-            spriteBatch.DrawString(font, "Score: " + score, new Vector2(10, 10), Color.Black);
-            spriteBatch.End();
-
-            base.Draw(gameTime);
-        }
+    public void Dispose()
+    {
+        Game.Components.Remove(this);
     }
 }
+
+public class Game1 : Game
+{
+    // ...
+
+    public void PlayExplosionEffect(Vector2 position)
+    {
+        // Trigger particle effect at the specified position
+        // Replace this with your actual particle effect triggering logic
+        // For example:
+        // explosionParticles.Position = position;
+        // explosionParticles.Trigger();
+    }
+
+    // ...
+}
+
